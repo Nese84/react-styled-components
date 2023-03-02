87544da4c6bb4081999c3bb970c019f1fe07f447
Nese84 <nbserbest84@gmail.com>
2023-03-02 22:53:44 +0300

    Nav

---
+++ src/components/styled/Nav.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
+  @media (max-width: 357px) {
+    text-align: center;
+  }
+`
+
+export default Nav
