1d1afa628bdffa9319a6d2bce5c2347adec83554
Nese84 <nbserbest84@gmail.com>
2023-03-02 22:33:24 +0300

    styled components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/styled/Button"
+
+const App = () => {
+  return (
+    <>
+      <Button color="#AF3A53">Apply Courses</Button>
+      <Button bg="#AF3A53">Talk To Adviser</Button>
+    </>
+  )
+}
+
+export default App
